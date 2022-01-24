4809a09980b9f49919369d47be327661e72e7704
NoixChou <shuto.tanaka.kos@gmail.com>
2022-01-25 01:40:51 +0900

    make init で Laravelバックエンドをcloneしていなかった

--- setup.sh
+++ setup.sh
@@ -7,3 +7,8 @@ git clone git@github.com:NoixChou/rust-sns-web.git
 cd rust-sns-web || exit
 cp .env.example .env
 cd ../ || exit
+
+git clone git@github.com:NoixChou/rust-sns-laravel-api.git
+cd rust-sns-laravel-api || exit
+cp .env.example .env
+cd ../ || exit
